233886650ce7ef2e9b062356c406beb8938f1e34
Kranzyo <100553582+Kranzyo@users.noreply.github.com>
2022-03-05 12:56:50 -0500

    safe measure

---  brackets v3 preset.lua
+++  brackets v3 preset.lua
@@ -4,26 +4,41 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR
 local Library = loadstring(game:GetObjects("rbxassetid://7974127463")[1].Source)()
 ]]
 
+repeat task.wait() until game.IsLoaded
+local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/TypeWriter.lua"))()
+if getgenv().hackExecuted then NotifyLib:TypeWrite("<font size=\"30\"><font color=\"rgb(63,126,252)\"><b>ⓘ</b></font></font> Script already executed!",1,0) return end
+getgenv().hackExecuted = true
+
+-- dependencies
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UserInputService = game:GetService("UserInputService")
+local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
+local PlayerService = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+
+-- creds AlexR32#0157 for modules
 local ConfigSystem = Debug and loadfile("Modules/ConfigSystem.lua")() or loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/ConfigSystem.lua"))()
 
 -- edit stuff below please don't forget
 local function SaveConfig() 
     if isfile("script config") then
-        ConfigSystem.WriteJSON(Config, "Kranz's Scripts/script config") -- PLEASE FUTURE ME EDIT THIS
+        ConfigSystem.WriteJSON(Config, "Kranz's Scripts/script config.JSON") -- PLEASE FUTURE ME EDIT THIS
     else
         makefolder("Kranz's Scripts")
-        ConfigSystem.WriteJSON(Config, "Kranz's Scripts/script config") -- PLEASE FUTURE ME EDIT THIS
+        ConfigSystem.WriteJSON(Config, "Kranz's Scripts/script config.JSON") -- PLEASE FUTURE ME EDIT THIS
     end
 end
 
 local function LoadConfig()
     if isfile("script config") then
-        getgenv().Config = ConfigSystem.ReadJSON("Kranz's Scripts/script config", Config)
+        getgenv().Config = ConfigSystem.ReadJSON("Kranz's Scripts/script config.JSON", Config)
     else
         makefolder("Kranz's Scripts")
-        ConfigSystem.WriteJSON(Config, "Kranz's Scripts/script config") -- PLEASE FUTURE ME EDIT THIS
+        ConfigSystem.WriteJSON(Config, "Kranz's Scripts/script config.JSON") -- PLEASE FUTURE ME EDIT THIS
     end
 end
+
 getgenv().Config = {
     UI = {
         Name = "Game",
@@ -35,17 +50,47 @@ getgenv().Config = {
         BackgroundColor = Color3.fromRGB(0,0,0),
         BackgroundTransparency = 0
     },
-}
 
+
+}
 LoadConfig()
+
 -- ui lib
 local Library = loadstring(game:GetObjects("rbxassetid://7974127463")[1].Source)()
-local Window = Library({Name = Config.UI.Name,Color = Color3.new(1,0.5,0.25),Size = UDim2.new(0,496,0,496),Position = UDim2.new(0.5,-248,0.5,-248)}) do
+local Window = Library({Name = Config.UI.Name,Enaled = Config.UI.Enabled, Color = Config.UI.color,Position = UDim2.new(0.5,-248,0.5,-248)}) do
     local MainTab = Window:AddTab({Name = "Main"}) do
         -- Scripts and stuff
 
     end
     local SettingsTab = Window:AddTab({Name = "Settings"}) do
+        local MainSection = SettingsTab:AddSection({Name = "Main Section", Side = "Left"}) do
+            MainSection:AddToggle({Name = "Enabled", value = Window.Enabled, Callback = function(State) 
+                Window:Toggle(State)
+            end}):AddBind({Key = Config.UI.Keybind,Callback = function(Bool, Key)
+                Config.UI.Keybind = Key or "NONE"
+            end, Blacklist = {"W","A","S","D","Slash","Tab","Backspace","Escape","Space","Delete","Unknown","Backquote"}})
+
+            MainSection:AddColorpicker({Name = "Color", Color = Window.Color, Callback = function(Color) 
+                Config.UI.Color = Color
+                Window:ChangeColor(Color)
+            end})
+        end
+
+        SettingsTab:AddButton({Name = "Server Hop",Side = "Left",Callback = function()
+            -- Credits to Infinite Yield for serverhop
+            local x = {}
+            for _, v in ipairs(HttpService:JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+                if type(v) == "table" and v.id ~= game.JobId then
+                    x[#x + 1] = v.id
+                end
+            end
+            if #x > 0 then
+                game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+            else
+                NotifyLib:TypeWrite("<font size=\"30\"><font color=\"rgb(252,126,63)\"><b>⚠</b></font></font> couldn't find a server",15,0)
+            end
+        end})
+
         local BackgroundSection = SettingsTab:AddSection({Name = "Background",Side = "Right"}) do
             BackgroundSection:AddDropdown({Name = "Image",Default = Config.UI.Background,
             List = {"Legacy","Hearts","Abstract","Hexagon","Circles","Lace With Flowers","Floral"},
@@ -99,9 +144,10 @@ local Window = Library({Name = Config.UI.Name,Color = Color3.new(1,0.5,0.25),Siz
         end
 
         local CreditsSection = SettingsTab:AddSection({"Credits Section", Side = "Right"}) do
+            CreditsSection:AddLabel({Text = "Thanks infinite yield for server hop"})
             CreditsSection:AddLabel({Text = "Thanks to Jan For This Awesome Patterns"})
             CreditsSection:AddLabel({Text = "Thanks to el3tric for Bracket V2 (Remade to Bracket V3.1) by AlexR32#0157"})
-            CreditsSection:AddLabel({Text = "Thanks AlexR32#0157 for config system and custom image background script"})
+            CreditsSection:AddLabel({Text = "Thanks AlexR32#0157 for config and notify system and custom image background script"})
             CreditsSection:AddLabel({Text = "And Kranz#0737 For Making This Awesome Script!"})
         end
     end
@@ -109,9 +155,9 @@ local Window = Library({Name = Config.UI.Name,Color = Color3.new(1,0.5,0.25),Siz
 end
 
 -- Config saving
-PlayerService.PlayerRemoving:Connect(function(Player))
+PlayerService.PlayerRemoving:Connect(function(Player)
     if Player == LocalPlayer then SaveConfig() end
-end
+end)
 
 --[[
 local Library = loadstring(game:GetObjects("rbxassetid://7974127463")[1].Source)()
